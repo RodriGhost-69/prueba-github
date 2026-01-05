5172ffeed031d9578d8869369498f2dfeb12f999
RodriGhost-69 <gutierrezperedorodrigo6@gmail.com>
2026-01-05 08:50:20 -0400

    Primer archivo principal

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola git");
+        System.out.println("Esta es la rama secundaria");
+
+        System.out.println("Esta es la prueba para el retroceso");
+    }
+}
